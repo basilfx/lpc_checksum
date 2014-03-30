912eaa0b7d12a5bf6a9f20bc7d828622050cbf48
Bas Stottelaar <basstottelaar@gmail.com>
2014-03-30 21:39:32 +0200

    Added setup.py. It can now be installed and directly used

---
+++ setup.py
@@ -0,0 +1,29 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+# Setup definitions
+setup(
+    name="lpc_checksum",
+    version="1.0",
+    description="Python script to calculate LPC firmware checksums",
+    author="Bas Stottelaar",
+    author_email="basstottelaar@gmail.com",
+    py_modules=["lpc_checksum"],
+    license = "MIT",
+    keywords = "lpc mcu cortex nxp",
+    entry_points={
+        "console_scripts": [
+            "lpc_checksum = lpc_checksum:main",
+        ]
+    },
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Embedded Systems",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+    ],
+)
