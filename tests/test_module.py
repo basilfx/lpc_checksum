ff10488652e250e906dec81cd8ad5aee9478bf20
Martin Hans <extmaha@bang-olufsen.dk>
2020-09-15 16:31:19 +0200

    Add test with hex file that has a non-zero start address
    This file fails because checksum() tries to read data starting at
    address 0.

--- tests/test_module.py
+++ tests/test_module.py
@@ -30,12 +30,23 @@ class LpcChecksumTest(unittest.TestCase):
 
         self.assertEqual(checksum, 0xefffe722)
 
+    def test_checksum__hex_high_start(self):
+        """
+        Test the `checksum` method using a HEX file where the start address is not zero.
+        """
+
+        path = os.path.join(
+            os.path.dirname(__file__), "data", "high-start-address.hex")
+        checksum = lpc_checksum.checksum(path, format="hex", read_only=True)
+
+        self.assertEqual(checksum, 0x53e48792)
+
     def test_checksum__overflow(self):
         """
         Test the `checksum` method given a binary value that generates an
-        overflow. 
-        
-        See https://github.com/basilfx/lpc_checksum/issues/2. 
+        overflow.
+
+        See https://github.com/basilfx/lpc_checksum/issues/2.
         """
 
         path = os.path.join(
